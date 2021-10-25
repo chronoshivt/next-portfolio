b0ac3e97a6ce57e2ebc280972d7bf7021c256e1c
chronoshivt <mokshafien@protonmail.com>
2021-10-24 21:07:01 -0500

    fixed icon and head

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-bg">
       <Head>
-        <title>Create Next App</title>
+        <title>Portfolio | RFD</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 

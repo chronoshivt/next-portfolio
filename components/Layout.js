bd2945284cec0d2cec1f5523a1d7d9087225e323
chronoshivt <mokshafien@protonmail.com>
2021-10-24 21:35:39 -0500

    bg color scheme

--- components/Layout.js
+++ components/Layout.js
@@ -1,13 +1,17 @@
 import Nav from './Nav'
 import Footer from './Footer'
 import Japan from './Japan'
+import Head from 'next/head'
 
 const Layout = ({children}) => {
 
         return (
             <div className="flex flex-col items-center justify-center min-h-screen py-2 pt-12 bg-bg overflow-hidden">
                 <main className="flex flex-col items-center justify-center w-full p-4 pt-12 flex-1 text-center sm:w-4/5 md:w-3/5 lg:w-2/5 xl:2/5">
-                    
+                <Head>
+                    <title>Portfolio | RFD</title>
+                    <link rel="icon" href="/favicon.ico" />
+                </Head>
                     <Nav />
 
                     <Japan />
--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import Layout from '../components/Layout'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
+    <Layout className="bg-bg">
 
       <Component {...pageProps} />
     

89ac4ee217136fc51dcfd8375083360a096f5d97
chronoshivt <mokshafien@protonmail.com>
2021-11-12 02:46:45 -0600

    added framer transitions

--- components/Article.js
+++ components/Article.js
@@ -1,35 +1,30 @@
 import React from 'react'
 import { motion } from 'framer-motion';
 
-const Article = ({children}) => {
-    return (
+const variants = {
+  hidden: {
+    y: 20,
+    opacity: 0,
+    x: 0
+  },
+  visible: {
+    y: 0,
+    opacity:1,
+     },
+}
+
+
+const Article = ({children}) => (
         <motion.div
         initial="hidden" animate="visible" 
-        variants={{
-          hidden: {
-            y: 20,
-            opacity: 0
-          },
-          visible: {
-            y: 0,
-            opacity:1,
-            animate:{
-              opacity: 0,
-            },
-            transition: {
-              duration: .4,
-              ease: "easeInOut"
-            }
-          },
-          exit: {
-            opacity: 0,
-            y:20,
-          }
-        }}
+        exit={{ opacity:0, y:20, x: -0}}
+        variants={variants}
+        transition={{ type:"easeInOut" , duration: 0.3}}
+        style={{ position: 'relative'}}
         >
             {children}
         </motion.div>
-    )
-}
+    
+)
 
 export default Article
--- pages/_app.js
+++ pages/_app.js
@@ -2,13 +2,13 @@ import { AnimatePresence } from 'framer-motion'
 import 'tailwindcss/tailwind.css'
 import Layout from '../components/Layout'
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, router }) {
   return (
     <Layout className="bg-bg">
 
 
       <AnimatePresence exitBeforeEnter initial={true}>
-      <Component {...pageProps} />
+      <Component {...pageProps} key={router.route} />
       </AnimatePresence>
     </Layout>
   ) 

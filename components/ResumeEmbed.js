0ed7bc5ad8ffb5250235775b45ac4a57da80ad61
howlsmoving <mokshafien@protonmail.com>
2022-01-12 17:09:59 -0600

    Update ResumeEmbed.js

--- components/ResumeEmbed.js
+++ components/ResumeEmbed.js
@@ -4,7 +4,7 @@ import React from 'react'
 function ResumeEmbed() {
     return (
         <div className="w-full -mt-8">
-          <iframe className="w-full" src="https://docs.google.com/document/d/e/2PACX-1vS9OHoGuOsa00Q_9Ea5yJpC8N6AYpT4AWqNC9QEr6Q93N3r20H95ykcri-K0d4Ae-a3DZaJrcdRK1B1/pub?embedded=true" height={320}></iframe>
+          <iframe className="w-full" src="https://docs.google.com/document/d/e/2PACX-1vS9OHoGuOsa00Q_9Ea5yJpC8N6AYpT4AWqNC9QEr6Q93N3r20H95ykcri-K0d4Ae-a3DZaJrcdRK1B1/preview" height={320}></iframe>
          </div>
     )
 }

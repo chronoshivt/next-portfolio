981f8d94211a044a2203de5bf95cc9dfe0660bf3
chronoshivt <75964136+chronoshivt@users.noreply.github.com>
2023-11-03 19:52:12 -0500

    add animation

--- components/Japan.js
+++ components/Japan.js
@@ -1,32 +1,76 @@
-import React from 'react'
-import Image from 'next/image'
-import japan from '../public/japan.png'
-import { Canvas } from "@react-three/fiber";
-import Floor from "../components/three/Floor";
-import Box from "../components/three/Box";
-import LightBulb from "../components/three/Lightbulb";
+import React, { useState, useEffect } from 'react';
+import { generateComplimentaryColors, generateRandomColorByDate } from './colors.js';
+
+function Art({ blobChars, blobColors }) {
+  const [blobPosition, setBlobPosition] = useState({ x: 25, y: 15 });
+  const [trailMap, setTrailMap] = useState(() => Array.from({ length: 30 }, () => new Array(100).fill(0)));
+
+  useEffect(() => {
+    const speed = Math.floor(Math.random() * (4 - 1 + 1)) + 1 // Constant speed for movement
+    const interval = setInterval(() => {
+      setBlobPosition(prevPosition => {
+        const angle = Math.random() * Math.PI * 2; // Generates random movement direction
+        const vx = Math.cos(angle) * speed;
+        const vy = Math.sin(angle) * speed;
+        const newX = Math.min(Math.max(prevPosition.x + vx, 0), 99);
+        const newY = Math.min(Math.max(prevPosition.y + vy, 0), 29);
+
+        // Update blob position and trail fade in one go
+        setTrailMap(prevMap => prevMap.map((row, y) => row.map((cell, x) => {
+          const distance = Math.hypot(newX - x, newY - y);
+          const fade = Math.max(0, cell - 0.02);
+          return distance < 3 ? Math.min(cell + 0.2, 1) : fade;
+        })));
+
+        return { x: newX, y: newY };
+      });
+    }, 100);
+
+    // Clean up the interval on component unmount
+    return () => clearInterval(interval);
+  }, []);
+
+  const artStyle = {
+    fontFamily: 'monospace',
+    whiteSpace: 'pre-wrap',
+    lineHeight: '8px',
+    fontSize: '16px',
+    letterSpacing: '2px',
+    height: '300px',
+  };
+  // Convert the trailMap to ASCII art
+  const generateAsciiArt = (trailMap) => {
+    return trailMap.map((row) =>
+      row.map((cell, x) => {
+        if (cell > 0) {
+          const opacityIndex = Math.floor(cell * (blobColors.length - 1));
+          const color = blobColors[opacityIndex];
+          const opacity = Math.floor(cell * 255).toString(16).padStart(2, '0');
+          const charIndex = Math.floor(cell * (blobChars.length - 1));
+          const char = blobChars[charIndex];
+          return `<span style="color:${color}${opacity};">${char}</span>`;
+        }
+        return ' ';
+      }).join('') + '\n'
+    ).join('');
+  };
 
 
-function Japan() {
-    return (
-        <div className="w-auto m-auto">
-          {/* <Image className="rounded-xl" src={japan} height={280} width={280}/> */}
-          <Canvas
-        shadows={true}
-        className="h-48"
-        camera={{
-          position: [-3, 4, 1],
-        }}
-      >
-                <ambientLight color={"green"} intensity={0.4} />
-                <LightBulb position={[-1, 4, 1]} />
-          <Box/>
+  const asciiArt = generateAsciiArt(trailMap);
 
-        {/* <Floor /> */}
-      </Canvas>
-
-        </div>
-    )
+  return <div style={artStyle} dangerouslySetInnerHTML={{ __html: asciiArt }} />;
 }
 
-export default Japan
+function Japan() {
+  const customChars = ['L', '▄', '^', '3', '\\', '/', 'e'];
+  const baseColor = generateRandomColorByDate(); // Reflects the current date in the base color
+  const complimentaryColors = generateComplimentaryColors(baseColor); // Color scheme based on base color
+
+  return (
+    <div className="w-full h-full overflow-hidden">
+      <Art blobChars={customChars} blobColors={complimentaryColors} />
+    </div>
+  );
+}
+
+export default Japan;
---
+++ components/colors.js
@@ -0,0 +1,106 @@
+function hexToHSL(hex) {
+    let r, g, b;
+    if (hex.length === 4) {
+        [r, g, b] = [1, 2, 3].map(i => parseInt(hex[i] + hex[i], 16));
+    } else if (hex.length === 7) {
+        [r, g, b] = [1, 3, 5].map(i => parseInt(hex.slice(i, i + 2), 16));
+    }
+
+    r /= 255;
+    g /= 255;
+    b /= 255;
+    let cmin = Math.min(r, g, b),
+        cmax = Math.max(r, g, b),
+        delta = cmax - cmin,
+        h, s, l = (cmax + cmin) / 2;
+
+    if (delta === 0) h = 0;
+    else {
+        h = ((cmax === r) ? ((g - b) / delta) % 6 : (cmax === g) ? (b - r) / delta + 2 : (r - g) / delta + 4) * 60;
+        if (h < 0) h += 360;
+    }
+
+    s = delta === 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+    
+    return [Math.round(h), +(s * 100).toFixed(1), +(l * 100).toFixed(1)];
+}
+
+function hslToHex(h, s, l) {
+    s /= 100;
+    l /= 100;
+    const a = s * Math.min(l, 1 - l);
+    const f = n => {
+        const k = (n + h / 30) % 12;
+        const color = l - a * Math.max(Math.min(k - 3, 9 - k, 1), -1);
+        return Math.round(255 * color).toString(16).padStart(2, '0');
+    };
+    return `#${f(0)}${f(8)}${f(4)}`;
+}
+
+export function generateComplimentaryColors(hex) {
+    let [h, s, l] = hexToHSL(hex);
+
+    // Define a range for the variations
+    let variationRange = 15; // degrees of variation in hue for a subtle change
+
+    // Generate shades (darker versions of the same hue)
+    let shades = [];
+    for (let i = 1; i <= 3; i++) {
+        shades.push(hslToHex(h, s, Math.max(l - i * variationRange, 0)));
+    }
+
+    // Generate tints (lighter versions of the same hue)
+    let tints = [];
+    for (let i = 1; i <= 3; i++) {
+        tints.push(hslToHex(h, s, Math.min(l + i * variationRange, 100)));
+    }
+
+    // Generate analogous colors (colors near the hue on the color wheel)
+    let analogous = [];
+    for (let i = -1; i <= 1; i++) {
+        if (i !== 0) {
+            let analogousHue = (h + i * variationRange + 360) % 360;
+            analogous.push(hslToHex(analogousHue, s, l));
+        }
+    }
+
+    // Generate a complimentary color (a color opposite on the color wheel)
+    let complimentary = hslToHex((h + 180) % 360, s, l);
+
+    // Construct the final palette
+    let palette = [...shades, hex, ...tints, complimentary, ...analogous];
+
+    // Sort the colors by their lightness values to have a nice gradient
+    palette.sort((a, b) => {
+        const [, , lightA] = hexToHSL(a);
+        const [, , lightB] = hexToHSL(b);
+        return lightA - lightB;
+    });
+
+    return palette;
+}
+
+
+
+
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
+export function generateRandomColorByDate() {
+    const seed = Date.now();
+    const random = (seed, max, factor = 1) => Math.floor((Math.sin(seed) * 10000 - Math.floor(Math.sin(seed) * 10000)) * max * factor);
+    return `#${random(seed, 70).toString(16).padStart(2, '0')}${random(seed + 1, 256).toString(16).padStart(2, '0')}${random(seed + 2, 70).toString(16).padStart(2, '0')}`;
+}
+
+// Example usage:
+// const color = generateRandomColorByDate();
+// console.log(color);
+
+// const originalColor = '#00ff00';
+// const complimentaryScheme = generateComplimentaryColors(originalColor);
+// console.log(complimentaryScheme);

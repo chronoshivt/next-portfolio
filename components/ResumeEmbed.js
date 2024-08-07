a7d4c39bc6590dec8447fb7e6c79d3b46a617632
chronoshivt <75964136+chronoshivt@users.noreply.github.com>
2024-08-06 19:13:49 -0500

    Update ResumeEmbed.js

--- components/ResumeEmbed.js
+++ components/ResumeEmbed.js
@@ -4,7 +4,7 @@ import React from 'react'
 function ResumeEmbed() {
     return (
         <div className="w-full -mt-8">
-          <iframe className="w-full" src="https://docs.google.com/document/d/1NclImeImImAU1LzGe2zZQzj3HIYYPwUcMpnWR97iTPw/preview" height={320}></iframe>
+          <iframe className="w-full" src="https://docs.google.com/document/d/10uHQWHSMTBuuBgjx0cYuyl2xEbow70BtEwPGnivJdo4/preview" height={640}></iframe>
          </div>
     )
 }
